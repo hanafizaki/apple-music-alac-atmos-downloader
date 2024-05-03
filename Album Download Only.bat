c73dc170c8fdf00fb5f8143357f79ce27244829b
hanafizaki <168611060+hanafizaki@users.noreply.github.com>
2024-05-03 11:10:29 +0800

    Add files via upload

---
+++ Album Download Only.bat
@@ -0,0 +1,15 @@
+@echo off
+setlocal
+cd "%root%"
+
+set /P URL1=Enter Url: %=%
+go run main.go %URL1%
+
+set loopcount=0
+:loop
+set /P URL=Enter Next Url: %=%
+go run main.go %URL%
+goto loop
+
+pause
+endlocal
